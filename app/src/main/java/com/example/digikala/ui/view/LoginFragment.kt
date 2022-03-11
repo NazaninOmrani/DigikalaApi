229a5e859d2b0772a9aeb66f106c50fcbfefced8
Nazanin Omrani <nazanin.omrani92@gmail.com>
2022-03-12 01:29:37 +0330

    clean

--- app/src/main/java/com/example/digikala/ui/view/LoginFragment.kt
+++ app/src/main/java/com/example/digikala/ui/view/LoginFragment.kt
@@ -1,7 +1,13 @@
 package com.example.digikala.ui.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
@@ -20,35 +26,131 @@ class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setOnClickViews()
         initViews(view)
     }
 
     private fun initViews(view: View) {
         navController = Navigation.findNavController(view)
-        button.setOnClickListener(this)
-        checkBox_save.setOnClickListener(this)
-        imageView2.setOnClickListener(this)
-        editText_name.setOnClickListener(this)
+        if (editTextPass.text?.equals(null) == false) {
+            frameShowPass.visibility = View.GONE
+        } else {
+            frameShowPass.visibility = View.VISIBLE
 
-        editText_name.setText(viewModel.getUserInfo()?.userName)
-        editText_pass.setText(viewModel.getUserInfo()?.password)
+        }
+        if (viewModel.getUserInfo() != null) {
+            editTextName.setText(viewModel.getUserInfo()?.userName)
+            editTextPass.setText(viewModel.getUserInfo()?.password)
+        }
 
+        checkBoxClick()
+
+        editTextName.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (viewModel.getUserInfo()?.userName != null &&
+                    viewModel.getUserInfo()?.userName != s.toString()
+                ) {
+                    checkBoxSaveInfo.visibility = View.VISIBLE
+                    checkBoxSaveInfo.text = getString(R.string.change_info)
+                } else if (viewModel.getUserInfo()?.userName == null && s.isNotEmpty()) {
+                    checkBoxSaveInfo.visibility = View.VISIBLE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        editTextPass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.isNotEmpty())
+                    frameShowPass.visibility = View.VISIBLE
+                else
+                    frameShowPass.visibility = View.GONE
+
+                if (viewModel.getUserInfo()?.password != null &&
+                    viewModel.getUserInfo()?.password != s.toString()
+                ) {
+                    checkBoxSaveInfo.visibility = View.VISIBLE
+                    checkBoxSaveInfo.text = getString(R.string.change_info)
+                } else if (viewModel.getUserInfo()?.password == null && s.isNotEmpty()) {
+                    checkBoxSaveInfo.visibility = View.VISIBLE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+        })
+    }
+
+    private fun setOnClickViews() {
+        buttonLogin.setOnClickListener(this)
+        frameShowPass.setOnClickListener(this)
+        editTextName.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.button -> navController.navigate(R.id.action_fragmentLogin_to_productFragment)
+            R.id.buttonLogin -> buttonClick()
 
-            R.id.checkBox_save -> {
-                if (checkBox_save.isChecked) {
-                    viewModel.saveUserInfo(
-                        UserInfo(
-                            editText_name.text.toString(),
-                            editText_pass.text.toString()
-                        )
-                    )
-                }
+            R.id.frameShowPass -> showPassIconClick()
+        }
+    }
+
+    private fun buttonClick() {
+        if (editTextName.text.toString().isEmpty() || editTextPass.text.toString().isEmpty()) {
+            Toast.makeText(context, R.string.please_inter_name, Toast.LENGTH_SHORT).show()
+        } else {
+            if (editTextName.text.toString() != viewModel.getUserInfo()?.userName || editTextPass.text.toString() != viewModel.getUserInfo()?.password) {
+                checkBoxSaveInfo.visibility = View.VISIBLE
+                if (checkBoxSaveInfo.isChecked)
+                    navController.navigate(R.id.action_fragmentLogin_to_productFragment)
+                else
+                    Toast.makeText(context, R.string.please_save_info, Toast.LENGTH_SHORT).show()
+            } else {
+                navController.navigate(R.id.action_fragmentLogin_to_productFragment)
             }
+        }
+    }
+
+    private fun checkBoxClick() {
+        checkBoxSaveInfo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                viewModel.saveUserInfo(
+                    UserInfo(
+                        editTextName.text.toString(),
+                        editTextPass.text.toString()
+                    )
+                )
+            } else {
+                Toast.makeText(context, R.string.please_save_info, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showPassIconClick() {
+        if (editTextPass.transformationMethod == HideReturnsTransformationMethod.getInstance()) {
+            editTextPass.transformationMethod = PasswordTransformationMethod.getInstance()
+            iconShowPass.setImageDrawable(context?.let {
+                AppCompatResources.getDrawable(
+                    it,
+                    R.drawable.ic_show_pass
+                )
+            })
+        } else {
+            editTextPass.transformationMethod =
+                HideReturnsTransformationMethod.getInstance()
+            iconShowPass.setImageDrawable(context?.let {
+                AppCompatResources.getDrawable(
+                    it,
+                    R.drawable.ic_hide_pass
+                )
+            })
         }
     }
 }
